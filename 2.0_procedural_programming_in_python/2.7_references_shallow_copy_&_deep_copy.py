746831a36c06f856d48abfe62505a77aa1195fc3
Pedro Clerici <pedro.clerici@tutamail.com>
2022-06-22 16:48:41 -0300

    Finished 2.7_references_shallow_copy_\&_deep_copy

--- 2.0_procedural_programming_in_python/2.7_references_shallow_copy_&_deep_copy.py
+++ 2.0_procedural_programming_in_python/2.7_references_shallow_copy_&_deep_copy.py
@@ -1,15 +1,26 @@
-# Dictionaries inside dictionaries:
-users = {
-    'user1': {
-        'name': 'steve',
-        'email': 'st3v3@example.com',
-    },
-    'user2': {
-        'name': 'scott',
-        'email': 'scott.brown@example.com',
-    },
-}
-for user_key, user_value in users.items():
-    print('\nShowing: ' + user_key)
-    for key, value in user_value.items():
-       print(f'{key}: {value}')
+import copy
+"""
+Reference, Shallow copy & Deep copy.
+"""
+
+my_array = ['Scott', 3.14, {'state': 'texas'}]
+
+# Reference example:
+my_array_reference = my_array
+my_array_reference[0] = 'Steve'
+print('my_array:', my_array)
+print('my_array_reference:', my_array_reference)
+
+# Shallow copy example:
+my_array_shallow_copy = my_array.copy()
+my_array_shallow_copy[0] = 'Maria'
+my_array_shallow_copy[-1]['state'] = 'California'
+print('my_array:', my_array)
+print('my_array_shallow_copy:', my_array_shallow_copy)
+
+# Deep copy example:
+my_array_deep_copy = copy.deepcopy(my_array)
+my_array_deep_copy[0] = 'Joseph'
+my_array_deep_copy[-1]['state'] = 'Florida'
+print('my_array:', my_array)
+print('my_array_deep_copy:', my_array_deep_copy)
