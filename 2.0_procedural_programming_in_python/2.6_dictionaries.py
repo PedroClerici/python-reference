4e4ed83541522d87dd806429413cb9710079c1e8
Pedro Clerici <pedro.clerici@tutamail.com>
2022-06-22 16:47:55 -0300

    Finished 2.6_dictionaries

--- 2.0_procedural_programming_in_python/2.6_dictionaries.py
+++ 2.0_procedural_programming_in_python/2.6_dictionaries.py
@@ -45,3 +45,19 @@ for key in random_dictionary:
 # or
 for key, value in random_dictionary.items():
   print(key, value, sep=': ')
+
+# Dictionaries inside dictionaries:
+users = {
+    'user1': {
+        'name': 'steve',
+        'email': 'st3v3@example.com',
+    },
+    'user2': {
+        'name': 'scott',
+        'email': 'scott.brown@example.com',
+    },
+}
+for user_key, user_value in users.items():
+    print('\nShowing: ' + user_key)
+    for key, value in user_value.items():
+       print(f'{key}: {value}')
